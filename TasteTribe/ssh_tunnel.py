429a9e7f7aeb2f93c6cedb70e4780fde37afdd67
andre <andreamonacelli02@gmail.com>
2024-07-17 15:39:11 +0200

    Configured connection to DB server

--- TasteTribe/ssh_tunnel.py
+++ TasteTribe/ssh_tunnel.py
@@ -1,12 +1,11 @@
 from sshtunnel import SSHTunnelForwarder
 
 server = SSHTunnelForwarder(
-    
-    # ssh_address_or_host="129.152.27.5",
-    # ssh_username="ubuntu",
-    # ssh_pkey="VPS-RICC",
-    # remote_bind_address=("localhost", 5432),
-    # local_bind_address=("localhost", 5432)
+    ssh_address_or_host="129.152.27.5",
+    ssh_username="ubuntu",
+    ssh_pkey="VPS-RICC",
+    remote_bind_address=("localhost", 5432),
+    local_bind_address=("localhost", 5432)
 )
 
 server.start()
