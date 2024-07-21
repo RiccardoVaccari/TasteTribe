a8502898ef9461e1abde3150fcdec4401f8e7d7b
andreamonacelli <andreamonacelli02@gmail.com>
2024-07-21 11:54:29 +0200

    - Removed userprofile app since it turned out useless - Started developing the page that shows recipe details - Defined a base template to be used as model for other templates

--- TasteTribe/settings.py
+++ TasteTribe/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     "login",
     "homepage",
     "quiz",
-    "userprofile",
     "recipedetail",
     "collectionz",
     "forum"
@@ -62,8 +61,7 @@ ROOT_URLCONF = 'TasteTribe.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- recipedetail/views.py
+++ recipedetail/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import *
 
-# Create your views here.
+
+# RECIPE DETAILS APP - VIEWS
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = "recipedetails/recipe_details.html"
+
+    def get_context_data(self, **kwargs):
+        pass  # Yet to be defined
+
+    def get_queryset(self):
+        return self.model.objects.filter(recipe_guid=self.request.GET.get("recipe_guid"))
