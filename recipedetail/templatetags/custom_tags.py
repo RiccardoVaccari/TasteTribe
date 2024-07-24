42746a52ef509713729c89037c62c87502217eb3
andre <andreamonacelli02@gmail.com>
2024-07-24 14:38:55 +0200

    RECIPE-DETAILS PAGE - a first preliminary page for guest users has been developed (backend-wise, the frontend still needs to be organized), moving onto the next feature

--- recipedetail/templatetags/custom_tags.py
+++ recipedetail/templatetags/custom_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from homepage.models import *
 from recipedetail.models import *
 
 register = template.Library()
@@ -7,8 +8,51 @@ register = template.Library()
 @register.inclusion_tag("recipedetails/recipe_steps.html")
 def render_recipe_steps(recipe_guid):
     try:
-        displayed_recipe = Recipe.objects.get(recipe_guid=recipe_guid)
         steps = RecipeStep.objects.filter(step_recipe_guid=recipe_guid).order_by("step_sequential_id")
-        return {"steps": steps, "recipe": displayed_recipe}
+        return {"steps": steps}
     except Recipe.DoesNotExist:
-        return {"steps": None, "recipe": None}
+        return {"steps": None}
+
+
+@register.inclusion_tag("recipedetails/recipe_tags.html")
+def render_recipe_related_tags(recipe_guid):
+    try:
+        tags_in_recipe = TagXRecipe.objects.filter(txr_recipe_guid=recipe_guid)
+        tags = []
+        for tag_x_recipe in tags_in_recipe:
+            tag_guid = tag_x_recipe.txr_tag_guid.tag_guid
+            tags.append(Tag.objects.get(tag_guid=tag_guid))
+        return {"tags": tags}
+    except Recipe.DoesNotExist:
+        return {"tags": None}
+
+
+@register.inclusion_tag("recipedetails/recipe_reviews.html")
+def render_recipe_reviews(recipe_guid):
+    try:
+        reviews = Review.objects.filter(review_recipe_guid=recipe_guid)
+        return {"reviews": reviews}
+    except Recipe.DoesNotExist:
+        return {"reviews": None}
+
+
+@register.inclusion_tag("recipedetails/recipe_ingredient_list.html")
+def render_recipe_ingredients(recipe_guid):
+    try:
+        ingredients_in_recipe = IngredientXRecipe.objects.filter(ixr_recipe_guid=recipe_guid)
+        ingredients = []
+        for ingredient in ingredients_in_recipe:
+            ingredient_guid = ingredient.ixr_ingredient_guid.ingredient_guid
+            dosage = ingredient.ixr_dosage_per_person
+            allergen_list = Allergen.objects.filter(allergen_id__in=ingredient.ixr_ingredient_guid.ingredient_allergens["allergen_ids"])
+            allergens = []
+            for allergen in allergen_list:
+                allergens.append(Allergen.objects.get(allergen_id=allergen.allergen_id))
+            ingredients.append({
+                "ingredient": Ingredient.objects.get(ingredient_guid=ingredient_guid),
+                "dosage": dosage,
+                "allergens": allergens
+            })
+        return {"ingredients": ingredients}
+    except Recipe.DoesNotExist:
+        return {"ingredients": None}
