b4844811b8b460e28ed0dec8c1c317dd3c7ee10f
Riccardo Vaccari <vaccari.riccardo02@gmail.com>
2024-09-04 17:50:27 +0200

    TESTING - Defined automated test for function check_ingredient in recipe detail

--- recipedetail/tests.py
+++ recipedetail/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Ingredient
+import uuid
 
-# Create your tests here.
+class CheckIngredientViewTest(TestCase):
+    def setUp(self):
+        self.ingredient1 = Ingredient.objects.create(
+            ingredient_guid=uuid.uuid4(),
+            ingredient_name="Sale",
+            ingredient_image="",
+            ingredient_allergens={}
+        )
+        self.ingredient2 = Ingredient.objects.create(
+            ingredient_guid=uuid.uuid4(),
+            ingredient_name="Zucchero",
+            ingredient_image="",
+            ingredient_allergens={}
+        )
+
+    def test_ingredient_exists(self):
+        response = self.client.get(reverse('check_ingredient'), {'ingredient': 'sale'})
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {'exists': True})
+
+    def test_ingredient_does_not_exist(self):
+        response = self.client.get(reverse('check_ingredient'), {'ingredient': 'pepe'})
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {'exists': False})
+
+    def test_empty_ingredient_query(self):
+        response = self.client.get(reverse('check_ingredient'), {'ingredient': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {'exists': False})
+
+    def test_no_ingredient_query(self):
+        response = self.client.get(reverse('check_ingredient'))
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {'exists': False})
